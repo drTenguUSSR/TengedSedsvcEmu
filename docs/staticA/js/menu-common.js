6a19767e794c42159d561a96ee74fc5c3cd90c99
drTengu <162064018+drTenguUSSR@users.noreply.github.com>
2024-03-13 01:50:57 +0300

    css redesign ,yaml parse ,"t5 hide buttons" added (proto)

--- docs/staticA/js/menu-common.js
+++ docs/staticA/js/menu-common.js
@@ -2,7 +2,7 @@
 
 const menuTop=document.getElementById("menu-top")
 const menuCmd=document.getElementById("menu-cmd")
-const menuClassSelected="button-menu__item_selected"
+const menuClassSelected="button_xmenu_selected"
 const requestType=document.getElementById("request-type")
 const requestText=document.getElementById("request-text")
 const fileAinputFile=document.getElementById("fileAinput")
@@ -72,15 +72,77 @@ some text ${id} 3
     }`;
 }
 
+function fileCfgInfo(fileLabel, fileData) {
+    let result=`fileUse: ${fileLabel}:`+" info: "
+    + fileData.info
+    + (('multiple' in fileData && fileData.multiple)?",files-multi":",file-one")
+    + ", extensions="+fileData.extensions
+    return result
+}
+
+function exampleRequestReset(event) {
+    console.log("exampleRequestReset-beg")
+    exampleRequestResetSub("fileAinput")
+    exampleRequestResetSub("fileBinput")
+    exampleRequestResetSub("fileCinput")
+    console.log("exampleRequestReset-end")
+}
+
+function exampleRequestResetSub(label) {
+    let elInf=document.getElementById(label+"Info")
+    elInf.innerText="";
+}
+
+//======================== mainFunc section
 function testYamlLoad(event) {
+    //topMenuInfo()
     console.log("testYamlLoad-beg")
-    const resultX=testYamlLoadSub()
-    console.log(resultX)
+    //const resultX=testYamlLoadSubA()
+    const resultX=testYamlLoad_2()
     console.log("testYamlLoad-end")
 }
 
-async function testYamlLoadSub() {
-    const response=await fetch("api/test-data-1.yaml")
+async function testYamlLoad_2() {
+    const response=await fetch("api/data-main.yaml")
+    const dataText=await response.text()
+    const ydoc = jsyaml.load(dataText);
+    const baseRel=ydoc.common['base-rel']
+    console.log("baseRel=["+baseRel+"]")
+
+    //selTab - выбранная вкладка из topMenu, содержит массив
+    const selTab = ydoc['tab-standart']
+    console.log("prop-beg")
+    
+    let tLabel=""
+    let tLen=0
+    //selSvc - описание конкретного сервиса
+    for(const selSvc of selTab) {
+        console.log("---")
+        console.log("svc.id="+selSvc.id)
+        console.log("svc.rel=!"+selSvc.rel+"!")
+        console.log("request["+selSvc.request+"]")
+        console.log("last=!"+selSvc['some-other']+"!")
+        const xLabel=selSvc.label
+        if(xLabel.length > tLen) {
+            tLabel=xLabel
+            tLen=tLabel.length
+        }
+        if ('fileA' in selSvc) {
+            console.log(fileCfgInfo('fileA',selSvc.fileA))
+        }
+        if ('fileB' in selSvc) {
+            console.log(fileCfgInfo('fileB',selSvc.fileB))
+        }
+    }
+
+    console.log("prop-end")
+    console.log(`max: ${tLabel}, len=${tLen}`)
+
+    return "xx1"
+}
+
+async function testYamlLoad_1() {
+    const response=await fetch("api/data-a.yaml")
     console.log("status="+response.status)
     const dataText=await response.text()
     let ydoc = jsyaml.load(dataText);
@@ -101,34 +163,65 @@ async function testYamlLoadSub() {
     return "xx1"
 }
 
-function clickFiles(event) {
+//========================
+function testQueryTopElements(event) {
+    console.log("testQueryTopElements-beg")
+    const filter = '[id^="tab-"]'
+    console.log(`filter:!${filter}!`)
+    const topAllBtn=menuTop.querySelectorAll(filter)
+    console.log(topAllBtn)
+    for(var elButton of topAllBtn) {
+        let bName=elButton.innerText
+        console.log(`id:${elButton.id} bName:${bName}`)
+    }
+    console.log("testQueryTopElements-end")
+}
+
+//========================
+const filterTopMenuButtons='[id^="tab-"]'
+const button_hide="button_hide"
+
+function testLoadYamlAndHideTop(event) {
+    console.log("testLoadYamlAndHideTop-beg")
+    const topAllBtnEl=menuTop.querySelectorAll(filterTopMenuButtons)
+    const topAllBtnId = new Set()
+    for(const elButtonEl of topAllBtnEl) {
+        topAllBtnId.add(elButtonEl.id)
+    }
+    console.log("topTab:"+Array.from(topAllBtnId).join(","))
+    testLoadYamlAndHideTop_s(topAllBtnEl,topAllBtnId)
+    console.log("testLoadYamlAndHideTop-end")
+}
+
+async function testLoadYamlAndHideTop_s(topAllBtnEl,topAllBtnId) {
+    const response=await fetch("api/data-main.yaml")
+    const dataText=await response.text()
+    let ydoc = jsyaml.load(dataText);
+    const baseRel=ydoc.common['base-rel']
+    console.log("baseRel=["+baseRel+"]")
+
+    const hideSect = new Set(topAllBtnId)
+    for(const sect in ydoc) {
+        console.log(`file-sect: ${sect}`)
+        hideSect.delete(sect)
+    }
+    console.log("hideSect list:"+Array.from(hideSect).join(","))
+    const childs=menuTop.childNodes
+    for(const elButtonEl of topAllBtnEl) {
+        //topAllBtnId.add(elButtonEl.id)
+        console.log(`hiding id=${elButtonEl.id} check=${hideSect.has(elButtonEl.id)}`)
+        if (hideSect.has(elButtonEl.id)) {
+            elButtonEl.classList.add(button_hide)
+        }
+    }
+    console.log("_s end")
+    return "xx1"
+}
+//======================== selFiles section
+function testFilesInfo(event) {
     console.log("clickFiles-BEG")
     console.log(fileAinputFile.files)
     console.log(fileBinputFile.files)
     console.log(fileCinputFile.files)
     console.log("clickFiles-END")
-}
-
-function changeFormAction(elName) {
-    console.log("form-action-beg")
-    console.log("elName=["+elName+"]")
-    let elForm=document.getElementById(elName)
-    console.log(elForm.method)
-    console.log(elForm.action)
-    elForm.action="http://localhost:8070/lost-space"
-    console.log(elForm.action)
-    console.log("form-action-end")
-}
-
-function exampleRequestReset(event) {
-    console.log("exampleRequestReset-beg")
-    exampleRequestResetSub("fileAinput")
-    exampleRequestResetSub("fileBinput")
-    exampleRequestResetSub("fileCinput")
-    console.log("exampleRequestReset-end")
-}
-
-function exampleRequestResetSub(label) {
-    let elInf=document.getElementById(label+"Info")
-    elInf.innerText="";
 }
