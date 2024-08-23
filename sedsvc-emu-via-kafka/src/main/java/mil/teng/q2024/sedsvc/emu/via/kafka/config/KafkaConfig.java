2a502706e04036502f681186bab98dba0f1b6658
drTengu <162064018+drTenguUSSR@users.noreply.github.com>
2024-08-23 22:57:07 +0300

    sedsvc-emu-via-kafka: disable autocreating topic && app must fail if topic not exist

--- sedsvc-emu-via-kafka/src/main/java/mil/teng/q2024/sedsvc/emu/via/kafka/config/KafkaConfig.java
+++ sedsvc-emu-via-kafka/src/main/java/mil/teng/q2024/sedsvc/emu/via/kafka/config/KafkaConfig.java
@@ -2,6 +2,11 @@ package mil.teng.q2024.sedsvc.emu.via.kafka.config;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.ListTopicsOptions;
+import org.apache.kafka.clients.admin.ListTopicsResult;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.boot.ssl.DefaultSslBundleRegistry;
@@ -12,18 +17,24 @@ import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 @Configuration
 @Slf4j
-public class KafkaConfig {
+public class KafkaConfig implements InitializingBean {
     private final KafkaProperties kafkaProperties;
-    private final String topicRcpt;
 
-    public KafkaConfig(KafkaProperties kafkaProperties, @Value("${sedSvc.topic-rcpt}") String topicRcpt) {
-        log.debug("KafkaConfig: .ctor. kProps={}, topicRcpt={}", kafkaProperties, topicRcpt);
+    @Value("${sedSvc.topic-rcpt}")
+    private String topicRcpt;
+
+    @Value("${spring.kafka.producer.bootstrap-servers}")
+    private String producerBootstrapServers;
+
+    public KafkaConfig(KafkaProperties kafkaProperties) {
+        log.debug("KafkaConfig: .ctor. kProps={}", kafkaProperties);
         this.kafkaProperties = kafkaProperties;
-        this.topicRcpt = topicRcpt;
     }
 
     @Bean
@@ -38,5 +49,25 @@ public class KafkaConfig {
     public KafkaTemplate<String, String> kafkaTemplate(ProducerFactory<String, String> producerFactory) {
         log.debug("kafkaTemplate: called. producerFactory={}", producerFactory);
         return new KafkaTemplate<>(producerFactory);
+    }
+
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        log.debug("afterPropertiesSet-beg. servers={}, topic={}", this.producerBootstrapServers, this.topicRcpt);
+
+        Map<String, Object> conf = new HashMap<>();
+        conf.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, producerBootstrapServers);
+        conf.put(AdminClientConfig.REQUEST_TIMEOUT_MS_CONFIG, "5000"); //TODO: fix hardcoded
+        try (AdminClient admin = AdminClient.create(conf)) {
+            ListTopicsOptions options = new ListTopicsOptions();
+            ListTopicsResult topicsResult = admin.listTopics(options);
+            Set<String> topics = topicsResult.names().get();
+            if (!topics.stream().anyMatch(it -> it.equals(this.topicRcpt))) {
+                throw new IllegalStateException("topic [" + this.topicRcpt + "] not found on broker "
+                        + producerBootstrapServers);
+            }
+        }
+        log.debug("afterPropertiesSet-end");
     }
 }
